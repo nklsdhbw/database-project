20d502a57c3c5fc651a80728ef7ab7dbf050bd76
Niklas <96066220+nklsdhbw@users.noreply.github.com>
2023-04-07 21:37:07 +0200

    added delete functionality in table, created scripts ordner to create sample database data

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import BootstrapTable from 'react-bootstrap-table-next';
 import Table from 'react-bootstrap/Table';
 
 function App() {
+  const api = "http://localhost:5000/run-query"
   const [results, setResults] = useState([]);
   const query = 'SELECT * FROM public."Librarians"';
 
@@ -24,19 +25,29 @@ function App() {
     return <p>Loading...</p>;
   }
 
-  const columns = [
-    { dataField: 'id', text: 'ID' },
-    { dataField: 'name', text: 'Name' },
-    { dataField: 'email', text: 'Email' },
-    { dataField: 'phone', text: 'Phone' },
-  ];
+  function deleteEntry(librarianID) {
+    axios
+      .post(api, {
+        query: `DELETE FROM public."Librarians" WHERE "librarianID" = ${librarianID}`
+      })
+      .then((response) => {
+        setResults(response.data);
+        console.log(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+  
 
   return (
     <Table striped bordered hover className="table mx-auto">
       <thead>
         <tr>
-          <th className="col">Gegenstand</th>
-          <th className="col">Menge</th>
+          <th className="col">ID</th>
+          <th className="col">Fullname</th>
+          <th className="col">Email</th>
+          <th className="col">Phone</th>
           <th className="col"></th>
         </tr>
       </thead>
@@ -45,8 +56,10 @@ function App() {
           <tr >
             <td>{entry[0]}</td>
             <td>{entry[1]}</td>
+            <td>{entry[2]}</td>
+            <td>{entry[3]}</td>
             <td>
-              <button className="w-100 btn btn-lg btn-primary">Delete</button>
+              <button className="w-100 btn btn-lg btn-primary" onClick={() => deleteEntry(entry[0])}>Delete</button>
             </td>
           </tr>
         ))}
