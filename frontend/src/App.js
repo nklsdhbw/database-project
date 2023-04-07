ffc03a5d9db3767d92d1cc306df091a47a40257a
Niklas <96066220+nklsdhbw@users.noreply.github.com>
2023-04-07 17:28:56 +0200

    initialized table

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,27 +1,58 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import axios from 'axios';
+import BootstrapTable from 'react-bootstrap-table-next';
+import Table from 'react-bootstrap/Table';
 
 function App() {
-  const [query, setQuery] = useState('');
   const [results, setResults] = useState([]);
+  const query = 'SELECT * FROM public."Librarians"';
 
-  const handleRunQuery = () => {
-    console.log(query)
-    axios.post('http://localhost:5000/run-query', { query })
-      .then(response => {
+  useEffect(() => {
+    axios
+      .post('http://localhost:5000/run-query', { query })
+      .then((response) => {
         setResults(response.data);
-        console.log(response.data)
+        console.log(response.data);
       })
-      .catch(error => {
+      .catch((error) => {
         console.log(error);
       });
-  };
+  }, []);
+
+  if (!results.length) {
+    // If there is no data available yet, show a loading indicator or an empty state.
+    return <p>Loading...</p>;
+  }
+
+  const columns = [
+    { dataField: 'id', text: 'ID' },
+    { dataField: 'name', text: 'Name' },
+    { dataField: 'email', text: 'Email' },
+    { dataField: 'phone', text: 'Phone' },
+  ];
 
   return (
-    <div>
-      <textarea value={query} onChange={(e) => setQuery(e.target.value)} />
-      <button onClick={handleRunQuery}>Run Query</button>
-    </div>
+    <Table striped bordered hover className="table mx-auto">
+      <thead>
+        <tr>
+          <th className="col">Gegenstand</th>
+          <th className="col">Menge</th>
+          <th className="col"></th>
+        </tr>
+      </thead>
+      <tbody>
+        {results.map((entry) => (
+          <tr >
+            <td>{entry[0]}</td>
+            <td>{entry[1]}</td>
+            <td>
+              <button className="w-100 btn btn-lg btn-primary">Delete</button>
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </Table>
   );
 }
-export default App
+
+export default App;
