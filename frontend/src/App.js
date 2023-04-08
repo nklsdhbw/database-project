7e72ecad5ff2c600ecf5aa3003d954e8fab07959
Niklas <96066220+nklsdhbw@users.noreply.github.com>
2023-04-09 01:14:29 +0200

    librarian records can be created and table updates

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
 import { useState, useEffect } from 'react';
+import { Modal, Form, Button } from "react-bootstrap";
 import axios from 'axios';
 import BootstrapTable from 'react-bootstrap-table-next';
 import Table from 'react-bootstrap/Table';
@@ -18,7 +19,6 @@ function App() {
   temp = temp + "ID"
   
   const [uniqueColumn, setUniqueColumn] = useState(temp);
-  //const query = `SELECT * FROM public."${selectedTable}"`;
   const [shouldRender, setShouldRender] = useState(false);
   const [updateData, setUpdateData] = useState(false);
 
@@ -83,6 +83,55 @@ useEffect(() => {
   
 
 
+  //new
+  const [formData, setFormData] = useState({
+    librarianID: { type: "number", required: true, placeholder: "123" },
+    librarianName: { type: "text", required: true, placeholder: "" },
+    librarianEmail: { type: "email", required: true, placeholder: "" },
+    librarianPhone: { type: "text", required: false, placeholder: ""},
+  });
+  
+    
+    const handleInputChange = (event) => {
+      const { name, value } = event.target;
+      setFormData({ ...formData, [name]: { ...formData[name], placeholder: value } });
+    };
+    
+  
+    function addEntry(librarianData) {
+        let valuesString = "VALUES("
+        let columnsString = ''
+        let table = '"Librarians"'
+        let values = Object.entries(librarianData).map(([key, value])=> (
+          valuesString = valuesString + `'${value.placeholder}',`
+        ))
+        let columns = Object.entries(librarianData).map(([key, value])=> (
+          columnsString = columnsString + `"${key}",`
+        ))
+        axios
+          .post(api, {
+            query: `INSERT INTO public.${table} (${columnsString.slice(0, columnsString.length-1)})`+ 
+            `${valuesString.slice(0, valuesString.length-1)}) `
+          })
+          .then((response) => {
+            //setResults(response.data);
+            console.log(response.data);
+            sessionStorage.setItem('updateData', !Boolean(JSON.parse(sessionStorage.getItem('updateData'))))
+            console.log(sessionStorage.getItem('updateData'))
+          })
+          .catch((error) => {
+            console.log(error);
+          });
+      }
+
+      const handleSubmit = (event) => {
+        event.preventDefault();
+        setFormData({ librarianID:"", librarianName: "", librarianEmail: "", librarianPhone: "" });
+        addEntry(formData)
+        setUpdateData(!updateData)
+      };
+
+
   if (!results.length) {
     // If there is no data available yet, show a loading indicator or an empty state.
     return <p>Loading...</p>;
@@ -134,12 +183,29 @@ useEffect(() => {
       </tbody>
     </Table>
     <div>
-    <button onClick={() => setShowModal(true)}>Create New Record</button>
-      <CreateRecordModal
-        show={showModal}
-        handleClose={handleCloseModal}
-        handleCreate={handleCreateRecord}
-      ></CreateRecordModal>
+    <button onClick={() => setShowModal(!showModal)}>Create New Record</button>
+    <Modal show={showModal} onHide={!showModal}>
+        <Modal.Header closeButton>
+          <Modal.Title>Create New Record</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form onSubmit={handleSubmit}>
+          {Object.entries(formData).map(([key, value])=> (
+  <Form.Group controlId={`${String(key)}`}>
+    <Form.Label>{`${String(key)}`}</Form.Label>
+    <Form.Control
+      type={`${String(value.type)}`}
+      name={`${String(key)}`}
+      value={`${value.placeholder}`}
+      onChange={handleInputChange}
+      required
+    />
+  </Form.Group>
+            ))}
+            <Button type="submit">Create</Button>
+          </Form>
+        </Modal.Body>
+      </Modal>
     </div>
     <div>
       <select id="mySelect" value={selectedTable} onChange={changeTable}>
