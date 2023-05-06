ffb503b9fe6b723b001e61682e262debb248b3ce
Niklas <96066220+nklsdhbw@users.noreply.github.com>
2023-05-06 22:15:21 +0200

    added activity "manage Publishers"

--- frontend/src/NavigationMenue.js
+++ frontend/src/NavigationMenue.js
@@ -71,6 +71,24 @@ const NavigationMenue = () => {
   JOIN "Currencies" c ON lo."libraryOrderCurrencyID" = c."currencyID"
   JOIN "Librarians" l ON lo."libraryOrderManagerLibrarianID" = l."librarianID";`,
     },
+    {
+      label: "Manage publishers",
+      table: "Publishers",
+      formQuery: `SELECT * FROM "Publishers"`,
+      entryQuery: `SELECT 
+      "publisherID" AS "Publisher ID",
+      "publisherName" AS "Name",
+      z."zipCode" AS "Zip",
+      z."zipCity" AS "City",
+        "publisherStreetName" AS "Street",
+      "publisherHouseNumber" AS "Housenumber",
+      "publisherCountry" AS "Country",
+      "publisherEmail" AS "Email",
+      "publisherPhone" AS "Phone"
+    FROM "Publishers" p
+    JOIN "ZIPs" z ON p."publisherZipID" = z."zipID"
+    `,
+    },
   ];
 
   const navigate = useNavigate();
