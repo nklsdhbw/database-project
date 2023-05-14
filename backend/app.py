10acc39f4ac1573af58ea6679057edd0ea3dbd7f
Niklas <96066220+nklsdhbw@users.noreply.github.com>
2023-05-14 22:19:30 +0200

    added sql scripts to be executed when container starts

--- backend/app.py
+++ backend/app.py
@@ -17,7 +17,7 @@ def run_query():
 
     conn = psycopg2.connect(
         host="localhost",
-        port=5432,
+        port=5438   ,
         database="postgres",
         user="postgres",
         password="0609"
