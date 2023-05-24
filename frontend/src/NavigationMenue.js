62fd6ec34ac9a4ed1769f18e234e8ff9d9a359b7
Niklas <96066220+nklsdhbw@users.noreply.github.com>
2023-05-24 12:38:06 +0200

    created View "enrichedBooks" and used it in "NavigationMenue" instead of using queries to represent the view

--- frontend/src/NavigationMenue.js
+++ frontend/src/NavigationMenue.js
@@ -112,24 +112,7 @@ const NavigationMenue = () => {
     {
       label: "Manage books",
       table: "Books",
-      entryQuery: `SELECT 
-      "bookID" AS "ID",
-  "bookTitle" AS "Title",
-  "bookISBN" AS "ISBN",
-   "authorFirstName" AS "Author firstname",
-      "authorLastName" AS "Author lastname",
-   "publisherName" AS "Publisher",
-    "categoryName" AS "Category",
-      "bookPublicationDate" AS "Publication date",
-      "bookAmount" AS "Amount (total)",
-   "bookAvailableAmount" AS "Amount (available)",
-      "bookAvailability" AS "Availability"
-  FROM 
-    "Books" b
-  JOIN "Authors" a ON a."authorID" = b."bookAuthorID"
-  JOIN "Categories" c ON c."categoryID" = b."bookCategoryID"
-  JOIN "Publishers" p ON p."publisherID" = b."bookPublisherID";
-    `,
+      entryQuery: `SELECT * FROM "enrichedBooks"`,
       formQuery: `SELECT * FROM "Books"`,
       img: bookManagement,
       read: ["Manager", "Employee", "Reader", "Admin"],
