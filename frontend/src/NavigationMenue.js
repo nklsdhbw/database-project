92aaa4bb2ef9e364dfce24133c625737b0399202
Niklas <96066220+nklsdhbw@users.noreply.github.com>
2023-05-24 12:33:29 +0200

    created View "enrichedLibrarians" and used it in "NavigationMenue" instead of using queries to represent the view

--- frontend/src/NavigationMenue.js
+++ frontend/src/NavigationMenue.js
@@ -92,25 +92,7 @@ const NavigationMenue = () => {
     {
       label: "Manage librarians",
       table: "Librarians",
-      entryQuery: `SELECT "librarianID" AS "ID", 
-      "librarianFirstName" AS "Firstname", 
-      "librarianLastName" AS "Lastname", 
-      "librarianEmail" AS "Email", 
-      "librarianPhone" AS "Phone", 
-      "librarianBirthDate" AS "Birth date",
-        "teamID" AS "Team ID",
-        
-        CASE
-            WHEN "librarianID" IN (SELECT "managerLibrarianID" FROM "Managers") THEN 'Manager'
-            WHEN "librarianID" IN (SELECT "employeeLibrarianID" FROM "Employees") THEN 'Employee'
-            ELSE 'Librarian'
-        END AS "Role"
-        
-    
-    FROM "Librarians" l
-    LEFT JOIN "Employees" e ON e."employeeLibrarianID" = l."librarianID"
-    LEFT JOIN "Managers" m ON m."managerLibrarianID" = l."librarianID"
-    LEFT JOIN "Teams" t ON "teamID" = e."employeeTeamID" OR "teamID" = m."managerTeamID"`,
+      entryQuery: `SELECT * FROM "enrichedLibrarians"`,
       formQuery: `SELECT * FROM "Librarians"`,
       img: employeeManagement,
       read: ["Manager", "Employee", "Reader", "Admin"],
