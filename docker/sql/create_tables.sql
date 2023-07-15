2f4a5d75a0d8375be94f946b3d7263ecb9d33b8a
Niklas <96066220+nklsdhbw@users.noreply.github.com>
2023-07-15 19:43:20 +0200

    removed bookCategoryID & bookPublicationDate not null constraint

--- docker/sql/create_tables.sql
+++ docker/sql/create_tables.sql
@@ -85,10 +85,10 @@ CREATE TABLE
         "bookAuthorID" INTEGER NOT NULL,
         "bookISBN" varchar(255) UNIQUE NOT NULL,
         "bookPublisherID" INTEGER NOT NULL,
-        "bookPublicationDate" DATE NOT NULL,
+        "bookPublicationDate" DATE,
         "bookAvailability" BOOLEAN NOT NULL,
         "bookAvailableAmount" INTEGER NOT NULL,
-        "bookCategoryID" INTEGER NOT NULL,
+        "bookCategoryID" INTEGER,
         CONSTRAINT fk_bookAuthorID FOREIGN KEY ("bookAuthorID") REFERENCES "Authors" ("authorID") ON DELETE CASCADE,
         CONSTRAINT fk_bookPublisherID FOREIGN KEY ("bookPublisherID") REFERENCES "Publishers" ("publisherID") ON DELETE CASCADE,
         CONSTRAINT fk_bookCategoryID FOREIGN KEY ("bookCategoryID") REFERENCES "Categories" ("categoryID") ON DELETE CASCADE
